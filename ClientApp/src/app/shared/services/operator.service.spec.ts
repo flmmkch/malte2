55af480b9e31362047f0e0e7c12d3c6a55ad3930
Victor Nivet <victor@saumon.ninja>
2022-02-19 22:47:21 +0100

    fix Angular tests

--- ClientApp/src/app/shared/services/operator.service.spec.ts
+++ ClientApp/src/app/shared/services/operator.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { OperatorService } from './operator.service';
@@ -6,7 +7,10 @@ describe('OperatorService', () => {
   let service: OperatorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [{ provide: 'BASE_URL', useValue: 'http://localhost/', deps: [] }]
+    });
     service = TestBed.inject(OperatorService);
   });
 
