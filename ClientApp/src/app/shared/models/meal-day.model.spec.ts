023bfecdc983641ba8b3a270dad1d02971ee620b
Victor Nivet <victor@saumon.ninja>
2022-05-08 22:50:22 +0200

    fix meal day Angular tests

--- ClientApp/src/app/shared/models/meal-day.model.spec.ts
+++ ClientApp/src/app/shared/models/meal-day.model.spec.ts
@@ -2,6 +2,6 @@ import { MealDay } from './meal-day.model';
 
 describe('MealDay', () => {
   it('should create an instance', () => {
-    expect(new MealDay()).toBeTruthy();
+    expect(new MealDay(undefined)).toBeTruthy();
   });
 });
