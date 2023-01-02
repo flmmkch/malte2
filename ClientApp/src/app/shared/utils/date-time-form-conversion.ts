ae57dabd703a3da57f1789267b8f0cd9488a83c6
Victor Nivet <victor@saumon.ninja>
2023-01-02 18:21:49 +0100

    fix date serialization timezone shift

--- ClientApp/src/app/shared/utils/date-time-form-conversion.ts
+++ ClientApp/src/app/shared/utils/date-time-form-conversion.ts
@@ -35,5 +35,5 @@ export function dateRangeFromDatepickerMonthYear(datepickerMonthYear: { month: n
 }
 
 export function dateToSerializationString(date: Date) {
-    return `${date.getUTCFullYear().toString().padStart(4, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    return `${date.getFullYear().toString().padStart(4, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 }
