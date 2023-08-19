deb2727cdbbd13d33eba394e97a6a6085bef9ecd
Victor Nivet <victor@saumon.ninja>
2023-08-19 13:56:31 +0200

    version 0.9.3

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { AccountingCategoriesComponent } from './modules/settings/accounting-cat
 import { FooterComponent } from './modules/footer/footer.component';
 import { RemissionsComponent } from './modules/remissions/remissions.component';
 
-export const APP_VERSION = '0.9.2';
+export const APP_VERSION = '0.9.3';
 
 @NgModule({
   declarations: [
